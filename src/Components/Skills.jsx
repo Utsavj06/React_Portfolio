700f6d315d7affe2f18655e746009b710404623e
Utsavj06 <utsav96jaiswal@gmail.com>
2022-07-01 02:33:18 +0530

    add skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,31 +1,49 @@
 import React from 'react';
 import "./Skills.css";
-import C from "../image/c++.jpg";
+import bootstrap from '../image/bootstrap.jpeg'
 import css from "../image/css.png";
 import JS from "../image/Js.png";
 import Reactj from "../image/Reactjs.png";
 import tailwind from "../image/tailwind.png";
-import JQuery from "../image/jquery.png";
+import JQuery from "../image/jquery.jpg";
+import Typescript from '../image/Typescript.png'
+import material from '../image/material-ui.png'
 
 
 const Skills = () => {
     return (
         <div className="Skills">
             <div className="center">
-                <img src={css} alt="C_and_C++" height="310" width="310" />
+                <img src={css} alt="HTML and CSS" height="200" width="200" />
                 <h1 className="">HTML and CSS</h1>
             </div>
             <div className="center">
-                <img src={JS} alt="C_and_C++" height="310" width="310" />
+                <img src={JS} alt="Javascript" height="200" width="200" />
                 <h1 className="">JavaScript</h1>
             </div>
             <div className="center">
-                <img src={Reactj}  alt="C_and_C++" height="310" width="310" />
+                <img src={Reactj}  alt="React.JS" height="200" width="200" />
                 <h1 className="">React Js</h1>
             </div>
             <div className="center">
-                <img src={tailwind} alt="C_and_C++" height="310" width="310" />
+                <img src={tailwind} alt="Tailwind CSS" height="200" width="200" />
                 <h1 className="">Tailwind CSS</h1>
+            </div> 
+            <div className="center">
+                <img src={Typescript} alt="Typescript" height="200" width="200" />
+                <h1 className="">Typescript</h1>
+            </div>
+            <div className="center">
+                <img src={material} alt="material" height="200" width="200" />
+                <h1 className="">Material UI</h1>
+            </div>
+            <div className="center">
+                <img src={bootstrap}  alt="bootstrap" height="200" width="200" />
+                <h1 className="">Bootstrap</h1>
+            </div>
+            <div className="center">
+                <img src={JQuery} alt="JQuery" height="200" width="200" />
+                <h1 className="">jQuery</h1>
             </div>               
         </div>
     )
