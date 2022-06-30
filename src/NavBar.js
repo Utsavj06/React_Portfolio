cb43bbd8ee1522eca5b76a6dbf90f6bad4fdef09
Utsavj06 <utsav96jaiswal@gmail.com>
2022-07-01 02:45:47 +0530

    better css

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,13 +11,13 @@ const NavBar = () => {
     return (
         <div className="parentName">
             <div className={hamburgger ? "mymenu mymenu-mobile":"mymenu"} onClick={()=>setHamburgger(false)}>
-                <Link exact to="/react_portfolio">Home</Link>
-                <Link exact to="/qualification">Qualification</Link>
-                <Link exact to="/skills">Skills</Link>
-                <Link exact to="/training_and_certification">Training/Certification</Link>
-                <Link exact to="/strengths">Strengths</Link>
-                <Link exact to="hobbies/">Hobbies</Link>
-                <Link exact to="/Pi">Personal Information</Link>
+                <Link exact to="/react_portfolio">Home |</Link>
+                <Link exact to="/qualification">Qualification |</Link>
+                <Link exact to="/skills">Skills |</Link>
+                <Link exact to="/training_and_certification">Training/Certification |</Link>
+                <Link exact to="/strengths">Strengths |</Link>
+                <Link exact to="hobbies/">Hobbies |</Link>
+                <Link exact to="/Pi">Personal Information |</Link>
             </div>
             <div className="hambugger" onClick={()=>setHamburgger(!hamburgger)}>
                 <GiHamburgerMenu />
